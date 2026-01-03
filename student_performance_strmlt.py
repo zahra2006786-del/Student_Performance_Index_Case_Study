d2d82cfb2117d87b279659195b84885f93ba5f4a
zahra2006786-del <zahra2006786@gmail.com>
2026-01-02 21:54:00 -0800

    Performance Index Streamlit App

--- student_performance_strmlt.py
+++ student_performance_strmlt.py
@@ -2,10 +2,11 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import joblib
-from sklearn.pipeline import Pipeline
+# from sklearn.pipeline import Pipeline
 
 
-Pipeline = joblib.load('LinearRegression.joblib')
+model = joblib.load('linear_model.joblib')
+scaler = joblib.load('scaler.joblib')
 
 st.markdown('# ***Know your performance***')
 df = pd.read_csv("Student_Performance.csv")
@@ -13,13 +14,15 @@ hour = st.selectbox('how many Hours you studied ?',df['Hours Studied'].unique())
 prev_score = st.number_input('What is your previous score ?')
 
 extra_act = st.selectbox('Are you involved in Extracurricular Activities ?', df['Extracurricular Activities'].unique())
-no = 0 if extra_act == 'False' else 1
+no = 0 if extra_act == False else 1
 
 sleep = st.selectbox('How many hours do you sleep ?', df['Sleep Hours'].unique())
 sample = st.selectbox('Number of Sample question papers you practiced ?', df['Sample Question Papers Practiced'].unique())
 
 pred = pd.DataFrame({'Hours Studied': [hour], 'Previous Scores': [prev_score], 'Extracurricular Activities': [no], 'Sleep Hours': [sleep], 'Sample Question Papers Practiced':[sample]})
+scaled = scaler.transform(pred)
 
 if st.button('Predict'):
-    index = Pipeline.predict(pred)
-    st.success(f"Your performance index is: {int(np.round(index, 0))}")
+    raw_index = model.predict(scaled)[0]
+    
+    st.success("Your performance index: " + str(int(round(raw_index, 0))))
